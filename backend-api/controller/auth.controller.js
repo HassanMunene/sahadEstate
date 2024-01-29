61cbed8c3ebdb770df09babb555ec195e7919e12
HassanMunene <awanzihassan@gmail.com>
2024-01-29 21:36:05 +0300

    create sign up api route

--- backend-api/controller/auth.controller.js
+++ backend-api/controller/auth.controller.js
@@ -1,4 +1,22 @@
-const signUpUser = (req, res) => {
-    res.json({msg: req.body});
+import User from '../models/user.model.js'
+import bcrypt from 'bcryptjs';
+
+const signUpUser = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+
+        if (!username || !email || !password) {
+            return res.status(400).json({msg: 'Missing required fields'})
+        }
+        // generating a salt for the password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const newUser = new User({username, email, password: hashedPassword});
+        await newUser.save();
+        res.status(201).json({msg: 'User created successfully'});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: error.message})
+    }
 }
 export default signUpUser;
