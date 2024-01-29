cd4e5033fa109ac297bf87a440030ac4513dccbb
HassanMunene <awanzihassan@gmail.com>
2024-01-29 11:13:26 +0300

    added tailwind css

--- clientSide/src/App.jsx
+++ clientSide/src/App.jsx
@@ -4,17 +4,21 @@ import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import About from "./pages/About"
 import Profile from "./pages/Profile"
+import Header from "./components/Header"
 
 const App = () => {
-  return <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Home />}/>
-    <Route path="/signIn" element={<SignIn />}/>
-    <Route path="/signUp" element={<SignUp />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/profile" element={<Profile />}/>
-  </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/sign_in" element={<SignIn />}/>
+      <Route path="/signUp" element={<SignUp />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />}/>
+    </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ clientSide/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <header className="bg-slate-200 shadow-md">
+            <div className="flex justify-between items-center max-w-6xl max-auto p-3">
+                <Link to='/'>
+                <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+                    <span className="text-slate-500">Sahand</span>
+                    <span className="text-slate-700">Estate</span>
+                </h1>
+                </Link>
+                <form className="bg-slate-100 flex items-center rounded-lg">
+                    <input type="text" placeholder="search..." className="bg-transparent p-3 focus:outline-none w-24 sm:w-64"/>
+                    <button className='p-3'>
+                        <FaSearch className='text-slate-600'/>
+                    </button>
+                </form>
+                <ul className='flex gap-4'>
+                    <Link to='/'><li className='text-slate-700 hover:underline cursor-pointer hidden sm:inline'>Home</li></Link>
+                    <Link to='/about'><li className='text-slate-700 hover:underline cursor-pointer hidden sm:inline'>About</li></Link>
+                    <Link to='/sign_in'><li className='text-slate-700 hover:underline cursor-pointer'>Sign in</li></Link>
+                </ul>
+            </div>
+        </header>
+    )
+}
+export default Header;
