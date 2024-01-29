27bb402476b65ae3eab6bf41c4a046aac58e7b6a
HassanMunene <awanzihassan@gmail.com>
2024-01-29 14:08:18 +0300

    connecting to the db

--- backend-api/index.js
+++ backend-api/index.js
@@ -1,6 +1,25 @@
 import express from 'express';
-const app = express()
+import dotenv from 'dotenv';
+import mongoose from "mongoose";
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
+const app = express()
+dotenv.config()
+const port = process.env.PORT || 5000
+
+/*
+========================
+database connections
+========================
+*/
+const connectDB = async (uri) => {
+    console.log('connecting to the database...')
+    return mongoose.connect(uri);
+}
+
+try {
+    // first connect application to the db
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is running on port ${port}`));
+} catch (error) {
+    console.log(error);   
+}
