ed8c8a45f7d065b43325dabaea8f38f54444e5c1
HassanMunene <awanzihassan@gmail.com>
2024-01-30 15:11:38 +0300

    sign up modal creation

--- clientSide/src/components/SignUpModal.jsx
+++ clientSide/src/components/SignUpModal.jsx
@@ -26,14 +26,40 @@ const SignUpModal = (props) => {
 
     return (
         <Modal isOpen={props.isOpen} onRequestClose={props.onClose} contentLabel="Sign Up Modal" className="modal">
-            <div className="modal-content">
-                <h2>Sign Up</h2>
-                <form>
-                    <input type="text" name="username" value={formData.username} onChange={handleChange} placeholder="username"/>
-                    <input type="email" name="email" value={formData.email} onChange={handleChange} placeholder="Email"/>
-                    <input type="password" name="password" value={formData.password} onChange={handleChange} placeholder="password"/>
-                    <button type="button">Sign Up</button>
+            <div className="modal-content p-8 max-w-3xl w-full h-80vh bg-white rounded-md shadow-lg mx-auto">
+                <h2 className="text-3xl text-center font-semibold my-7">Sign Up</h2>
+                <form onSubmit={handleSubmit} className="flex flex-col gap-5">
+                    <input 
+                        type="text" 
+                        name="username" 
+                        value={formData.username} 
+                        onChange={handleChange} 
+                        placeholder="username"
+                        className="border p-3 rounded-lg"
+                        id="username"
+                    />
+                    <input 
+                        type="email" 
+                        name="email" 
+                        value={formData.email} 
+                        onChange={handleChange} 
+                        placeholder="Email"
+                        className="border p-3 rounded-lg"
+                        id="email"
+                    />
+                    <input 
+                        type="password" 
+                        name="password" 
+                        value={formData.password} 
+                        onChange={handleChange} 
+                        placeholder="password"
+                        className="border p-3 rounded-lg"
+                    />
+                    <button type="button" className="bg-slate-700 p-3 text-white uppercase rounded-lg hover:opacity-95 disabled:opacity-80">Sign Up</button>
                 </form>
+                <div className="mt-5">
+                    <p>Have an account? <span className="text-green-700 cursor-pointer">Sign in</span></p>
+                </div>
             </div>
         </Modal>
     );
