66755b84c76818b7d0a1b204e75a43f62fcd67d7
HassanMunene <awanzihassan@gmail.com>
2024-01-31 15:34:11 +0300

    setting up sign up and sign in modal

--- clientSide/src/App.jsx
+++ clientSide/src/App.jsx
@@ -12,18 +12,30 @@ const App = () => {
   const [isSignUpModalOpen, setSignUpModalOpen] = useState(false);
   const [isSignInModalOpen, setSignInModalOpen] = useState(false);
 
-  const openSignUpModal = () => setSignUpModalOpen(true);
+  // open the sign up modal
+  const openSignUpModal = () => {
+    setSignInModalOpen(false);
+    return setSignUpModalOpen(true)
+  };
+
+  // close the sing up modal
   const closeSignUpModal = (data) => {
-    console.log(data);
     if (data.success === true) {
       setSignUpModalOpen(false);
       setSignInModalOpen(true);
     } else {
-      return setSignUpModalOpen(true)
+      return setSignUpModalOpen(false);
     }
   };
 
-  const openSignInModal = () => setSignInModalOpen(true);
+  // open sign in modal
+  const openSignInModal = () => {
+    console.log('Should open sign in modal and close sign up modal');
+    setSignUpModalOpen(false);
+    return setSignInModalOpen(true);
+  }
+
+  // close sing in modal
   const closeSignInModal = () => setSignInModalOpen(false);
 
   return (
@@ -35,8 +47,8 @@ const App = () => {
       <Route path="/about" element={<About />} />
       <Route path="/profile" element={<Profile />}/>
     </Routes>
-    <SignUpModal isOpen={isSignUpModalOpen} onClose={closeSignUpModal}/>
-    <SignInModal isOpen={isSignInModalOpen} onClose={closeSignInModal}/>
+    <SignUpModal isOpen={isSignUpModalOpen} onClose={closeSignUpModal} openSignIn={openSignInModal}/>
+    <SignInModal isOpen={isSignInModalOpen} onClose={closeSignInModal} openSignUp={openSignUpModal}/>
     </BrowserRouter>
   )
 }
