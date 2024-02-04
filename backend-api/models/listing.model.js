65e1ead15f8b8ddbc9a57abcb2c5ff788d90bc2e
HassanMunene <awanzihassan@gmail.com>
2024-02-04 15:43:54 +0300

    listings database model

---
+++ backend-api/models/listing.model.js
@@ -0,0 +1,62 @@
+import mongoose, { Schema, trusted } from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const listingSchema = Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    regularPrice: {
+        type: Number,
+        required: true
+    },
+    discountedPrice: {
+        type: Number,
+        required: true
+    },
+    bathrooms:{
+        type: Number,
+        required: true
+    },
+    bedrooms: {
+        type: Number,
+        required: true
+    },
+    furnished: {
+        type: Boolean,
+        required: true
+    },
+    parking: {
+        type: Boolean,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    offer: {
+        type: Boolean,
+        required: true
+    },
+    imageURL: {
+        type: Array,
+        required: true
+    },
+    userRef: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true})
+
+const Listing = model('Listing', listingSchema);
+
+export default Listing
