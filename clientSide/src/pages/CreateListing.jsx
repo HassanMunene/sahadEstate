dfefdd49eff113e81c5bd3d25de028a2f8b41f55
HassanMunene <awanzihassan@gmail.com>
2024-02-05 09:34:10 +0300

    complete create listing page UI

--- clientSide/src/pages/CreateListing.jsx
+++ clientSide/src/pages/CreateListing.jsx
@@ -3,8 +3,8 @@ const CreateListing = () => {
         <div className="px-6">
             <main className="max-w-4xl mx-auto">
                 <h1 className="capitalize text-3xl font-semibold text-center my-7">create Listing</h1>
-                <form className="flex flex-col sm:flex-row">
-                    <div className="flex flex-col gap-4 flex-1">
+                <form className="flex flex-col sm:flex-row gap-4">
+                    <div className="left-col flex flex-col gap-4 flex-1">
                         <input type="text" name="name" id="name" placeholder="Name" className="border p-3 rounded-lg" maxLength="62" minLength="10" required/>
                         <textarea type="text" name="description" id="description" placeholder="Description" className="border p-3 rounded-lg" required/>
                         <input type="text" name="address" id="address" placeholder="Address" className="border p-3 rounded-lg"/>
@@ -30,24 +30,42 @@ const CreateListing = () => {
                                 <span>Offer</span>
                             </div>
                         </div>
-                        <div className="">
+                        <div className="flex flex-wrap gap-6">
                             <div className="flex items-center gap-2">
-                                <input type="number" name="bedrooms" id="bedrooms" min="1" max="10" required className="p-3 border border-gray-300 rounded-lg w-16"/>
+                                <input type="number" name="bedrooms" id="bedrooms" min="1" max="10" required className="w-16 p-2 border border-gray-300 rounded-lg"/>
                                 <p>Beds</p>
                             </div>
                             <div className="flex items-center gap-2">
-                                <input type="number" name="bathrooms" id="bathrooms" min="1" max="10" required className="p-3 border border-gray-300 rounded-lg w-16"/>
+                                <input type="number" name="bathrooms" id="bathrooms" required className="w-16 p-2 border border-gray-300 rounded-lg"/>
                                 <p>Baths</p>
                             </div>
                             <div className="flex items-center gap-2">
-                                <input type="number" name="regularPrice" id="regularPrice" min="1" max="10" required className="p-3 border border-gray-300 rounded-lg w-16"/>
-                                <p>Regular price</p>
+                                <input type="number" name="regularPrice" id="regularPrice" required className="w-24 p-2 border border-gray-300 rounded-lg"/>
+                                <div className="flex flex-col items-center">
+                                    <p>Regular price</p>
+                                    <span className="text-xs">(Ksh / month)</span>
+                                </div>
                             </div>
                             <div className="flex items-center gap-2">
-                                <input type="number" name="discountedPrice" id="discountedPrice" min="1" max="10" required className="p-3 border border-gray-300 rounded-lg w-16"/>
-                                <p>Baths</p>
+                                <input type="number" name="discountedPrice" id="discountedPrice" min="1" max="10" required className="w-24 p-2 border border-gray-300 rounded-lg"/>
+                                <div className="flex flex-col items-center">
+                                    <p>Discounted price</p>
+                                    <span className="text-xs">(Ksh / month)</span>
+                                </div>
                             </div>
                         </div>
+                    </div>
+                    <div className="right-col flex flex-col flex-1 gap-4">
+                        <p className="font-semibold">Images: 
+                        <span className="font-normal text-gray-600 ml-2">The first image will be cover (max 6)</span>
+                        </p>
+                        <div className="flex gap-4">
+                            <input type="file" id="images" name="images" accept="image/*" multiple className="p-3 border border-gray-300 rounded w-full"/>
+                            <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">upload</button>
+                        </div>
+                        <button className="uppercase p-3 bg-slate-700 text-white rounded-lg hover:opacity-95 disabled:opacity-80">
+                            create listing
+                        </button>
                     </div>
                 </form>
         </main>
