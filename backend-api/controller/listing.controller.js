45aeb34aeaf4f3b819304d24eacc71b8e9388fb0
HassanMunene <awanzihassan@gmail.com>
2024-02-06 18:25:34 +0300

    deleting specific listing

--- backend-api/controller/listing.controller.js
+++ backend-api/controller/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import errorFunction from "../utils/error.js";
 
 export const testingListing = (req, res, next) => {
     res.json('testing listings');
@@ -9,6 +10,25 @@ export const createListing = async (req, res, next) => {
         res.status(201).json(listing);
     } catch (error) {
         console.log(error);
+        next(error);
+    }
+}
+
+export const deleteListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+    console.log(listing.name);
+    if (!listing) {
+        return next(errorFunction(404, 'listing not found'));
+    }
+    if (req.user.id !== listing.userRef) {
+        console.log(req.user.id, listing.userRef)
+        return next(errorFunction(401, 'not authorized to delete the listing'));
+    }
+    
+    try {
+        await Listing.findByIdAndDelete(req.params.id);
+        res.status(200).json('successfully deleted')
+    } catch (error) {
         next(error);
     }
 }
--- backend-api/routes/listing.route.js
+++ backend-api/routes/listing.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { testingListing, createListing } from '../controller/listing.controller.js';
+import { testingListing, createListing, deleteListing } from '../controller/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const listingRouter = express.Router();
 
 listingRouter.route('/testing').get(verifyToken, testingListing);
 listingRouter.route('/create_listing').post(verifyToken, createListing);
+listingRouter.route('/:id').delete(verifyToken, deleteListing)
 
 export default listingRouter;
