916228882c950326efc6be445acd94230f131cc3
HassanMunene <awanzihassan@gmail.com>
2024-02-08 18:28:27 +0300

    implementing the search functionality

--- backend-api/controller/listing.controller.js
+++ backend-api/controller/listing.controller.js
@@ -1,3 +1,4 @@
+import { query } from "express";
 import Listing from "../models/listing.model.js";
 import errorFunction from "../utils/error.js";
 
@@ -61,4 +62,49 @@ export const getListing = async (req, res, next) => {
         console.log(error);
         next(error);
     }  
+}
+
+export const getListings = async (req, res, next) => {
+    try {
+        const limit = parseInt(req.query.limit) || 9;
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        let offer = req.query.offer;
+        if (offer === undefined || offer === false) {
+            offer = { $in: [false, true] };
+        }
+
+        let furnished = req.query.furnished; 
+        if (furnished === undefined || furnished === false) {
+            furnished = { $in: [false, true] };
+        }
+
+        let parking = req.query.parking;
+        if (parking === undefined || parking === false) {
+            parking = { $in: [false, true]};
+        }
+
+        let type = req.query.type;
+        if (type === undefined || type === 'all') {
+            type = { $in: ['rent', 'sale'] };
+        }
+        const searchTerm = req.query.searchTerm || '';
+        const sort = req.query.sort || 'createdAt';
+        const order = req.query.order || 'desc';
+
+        const listings = await Listing.find({
+            name: { $regex: searchTerm, $options: 'i'},
+            offer,
+            furnished,
+            parking,
+            type,
+        }).sort(
+            {[sort]: order}
+        ).limit(limit).skip(startIndex);
+
+        res.status(200).json(listings);
+
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
 }
